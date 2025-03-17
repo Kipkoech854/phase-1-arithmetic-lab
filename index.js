bec3b2a61770d515dd3e9d1d1d25cb5f7afe7daf
Gideon kipkoech <gideon.kipkoech1@student.moringaschool.com>
2025-03-17 23:58:44 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 10  );
+ const num3 = 104;
+ const num4 = 25;
+const mod = num3 % num4;
+const numbers = (5, 7, 12, 13, 14, 17, 20);
+const max = Math.max(5, 7, 12, 13, 14, 17, 20);
 
-// Write your code here
